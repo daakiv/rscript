f0ba224a8b6ca58b723d9790291e3e4ad555bf25
David Amadi <daakiv27@gmail.com>
2022-11-08 21:45:52 +0300

    new function

--- mdata.R
+++ mdata.R
@@ -4,6 +4,10 @@ library(devtools)
 library(ggsn)
 library(ggplot2)
 library(foreign)
+library(tidyr)
 
 ####Loading the mosquito data
 data_points <- read.dta("/Users/damadi/MYDATA/maaping/longitude.dta")
+####Load KHDSS locations shapefile
+shapefile2 <- read.dta("/Users/damadi/MYDATA/maaping/locations-c.dta")
+colnames(shapefile2)[1] <- "id" ##renaming variable name from _ID to id
